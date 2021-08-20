bf3f67f0fdd3bd71b686f585dd861cc348ec96a8
blaise-m <mubangiziblaise@gmail.com>
2021-08-20 14:20:01 +0300

    Add CORS Settings

--- resume/settings.py
+++ resume/settings.py
@@ -30,10 +30,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
